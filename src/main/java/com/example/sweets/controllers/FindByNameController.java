01f2a53ed23378e5748b691058cc09f6a3c5f3c9
Evelina <goblin.voblin09@gmail.com>
2024-02-24 21:30:35 +0100

    Merge pull request #4 from evelina0001/find_by_sugar
    find by name

---
+++ src/main/java/com/example/sweets/controllers/FindByNameController.java
@@ -0,0 +1,61 @@
+package com.example.sweets.controllers;
+
+import com.example.sweets.models.DB;
+import com.example.sweets.models.Gift;
+import com.example.sweets.models.Sweet;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FindByNameController implements Initializable {
+    @FXML
+    private TableView<Sweet> tableView;
+    @FXML
+    private TextField Name;
+
+
+        @FXML
+        private TableColumn<Sweet, String> idColumn;
+        @FXML
+        private TableColumn<Sweet, String> nameColumn;
+        @FXML
+        private TableColumn<Sweet, String> priceColumn;
+        @FXML
+        private TableColumn<Sweet, String> amountColumn;
+        @FXML
+        private TableColumn<Sweet, String> sugarColumn;
+        Controller controller = new Controller();
+
+    @FXML
+        public void goBackClick(ActionEvent event) throws IOException {
+            controller.goTo(event, "src/main/resources/com/example/sweets/gift-view.fxml");
+        }
+        @FXML
+        public void onFindClick(ActionEvent event) {
+            Gift gift = DB.gift;
+            DB db = new DB();
+            gift.setPresent(db.getSweets(DB.gift.getId()));
+            tableView.setItems(FXCollections.observableArrayList(gift.findByName(Name.getText())));
+            if (Name.getText().isEmpty()) {
+                controller.showMessage("Write a name");
+            }
+        }
+        @Override
+        public void initialize(URL url, ResourceBundle resourceBundle) {
+            controller.setCells(idColumn, nameColumn, priceColumn, sugarColumn, amountColumn);
+            DB db = new DB();
+            ObservableList<Sweet> sweets = FXCollections.observableArrayList(db.getSweets(DB.gift.getId()));
+            tableView.setItems(sweets);
+        }
+    }
+
